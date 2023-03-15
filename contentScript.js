e19c4479ad4994459d63b5f03304ed3a4d3f4a3d
guyuedumingx <guyuedumingx@gmail.com>
2023-03-15 17:30:14 +0800

    rename contentScript2 to contentScript

--- contentScript.js
+++ contentScript.js
@@ -1,135 +1,181 @@
-let isShow = false;
+let mode = 0; //0表示normal模式 1: search模式
+let isShow = false; //表示窗口有没有显示
+let bindings = "abcdehijklmnopqrstuvwxyz".split(""); // 可以映射成关键字的字母
+var data = {}; //所有数据
 let activeElement = null;
-let bindings = "abcdehijklmnopqrstuvwxyz";
-
-function getKeys(count) {
-  let preffix = "";
-  let res = [];
-  let p = 0;
-  while (true) {
-    for (let i = 0; i < bindings.length; i++) {
-      res.push(preffix + bindings[i]);
-      p++;
-      if (p === count) {
-        return res;
-      }
-    }
-    preffix = res.shift();
-    p--;
-  }
-}
-
-const rowMaxCount = 25;
-
-const itemMap = {};
-
-const buildHelpWindow = (div, data) => {
-  activeElement = document.activeElement;
-  //   console.log(activeElement);
-  //   let left = activeElement.offsetLeft;
-  //   let top = activeElement.offsetTop;
-  //   let height = activeElement.offsetHeight;
-  //   console.dir(activeElement);
-  //   let bottom = top + height;
-  //   div.style.left = `${left}px`;
-  //   div.style.top = `${top + height + 5}px`;
-  div.id = "my-popup";
-  let keys = Object.keys(data);
-  let colCount = Math.ceil(keys.length / rowMaxCount);
-  for (let i = 0; i < colCount; i++) {
-    const aaa = document.createElement("div");
-    aaa.setAttribute("class", "column");
-    for (let j = 0; j < rowMaxCount; j++) {
-      let key = keys[i * rowMaxCount + j];
-      if (!key) {
-        break;
-      }
-      let name = Object.keys(data[key])[0];
-      let value = Object.values(data[key])[0];
-      const item = document.createElement("div");
-      item.setAttribute("class", "form-input-box");
-      item.data = value;
-      item.addEventListener("click", (e) => {
-        e.preventDefault();
-        handler(value);
-      });
-      itemMap[key] = item;
-      item.innerHTML = `
-                <abcd class="key">${key}</abcd>
-                <abcd class="value">${name}</abcd>
-            `;
-      aaa.appendChild(item);
-    }
-    div.appendChild(aaa);
-  }
-  return div;
-};
-
-let data = {};
-const closeHelpWindow = () => {
-  // 如果弹窗已经显示，则删除弹窗元素并将标记变量设置为 false
-  const popup = document.querySelector("#my-popup");
-  popup.parentNode.removeChild(popup);
-  isShow = false;
-  data = {};
-};
-
-const handler = (value) => {
-  if (!activeElement) {
-    return;
-  }
-  activeElement.value = value;
-  activeElement.dispatchEvent(new Event("change"));
-  activeElement.dispatchEvent(new Event("input"));
-  closeHelpWindow();
-};
-let pref = "";
+let windowElement = null;
+let curKey = "";
+let key2IndexMap = {};
+let pinyin2IndexMap = {};
 
 document.addEventListener("keydown", function (event) {
-  // 获取存储在插件中的配置
+  console.log(mode);
   if (chrome.runtime?.id) {
+    //读取存储中的信息
     chrome.storage.sync.get().then((map) => {
-      let keys = getKeys(Object.keys(map).length);
-
+      //   data = map.sort((a, b) => {
+      //     a.count - b.count;
+      //   });
       if (event.key === "i" && event.altKey) {
-        if (isShow) {
-          closeHelpWindow();
-        } else {
-          let p = 0;
-          for (let key in map) {
-            let index = keys[p++];
-            data[index] = {
-              [key]: map[key],
-            };
+        toggleWindow(map);
+        return;
+      } else if (event.key === "/") {
+        toggleSearchMode();
+        return;
+      } else if (event.key === "Backspace") {
+        curKey = curKey.slice(0, curKey.length - 1);
+      }
+      if (isShow) {
+        if (bindings.includes(event.key)) curKey += event.key;
+        highlight();
+        if (mode == 1) {
+          //search
+          let tmpData = {};
+          for (let key in data) {
+            if (data[key].pinyin.startsWith(curKey)) tmpData[key] = data[key];
           }
-          console.log(data);
-          const div = buildHelpWindow(document.createElement("div"), data);
-          document.body.appendChild(div);
-          isShow = true;
+          updateWindowContent(windowElement, tmpData);
+          return;
         }
-      } else if (isShow) {
-        if (bindings.includes(event.key)) {
-          pref += event.key;
-          if (keys.includes(pref)) {
-            let value = Object.values(data[pref])[0];
-            handler(value);
-            pref = "";
-          } else {
-            for (let i = 0; i < keys.length; i++) {
-              if (keys[i].startsWith(pref)) {
-                itemMap[keys[i]].setAttribute(
-                  "class",
-                  "form-input-box heightlight"
-                );
-              } else {
-                itemMap[keys[i]].setAttribute("class", "form-input-box");
-              }
-            }
-          }
-        }
-      } else if (event.keyCode == 27 && isShow) {
-        closeHelpWindow();
+        //normal
+        if (key2IndexMap[curKey])
+          inputContentToActiveElement(data[key2IndexMap[curKey]]);
       }
     });
   }
 });
+
+const highlight = () => {
+  //normal模式搜索key字段
+  let s = "key";
+  //search模式搜索pinyin字段
+  if (mode == 1) s = "pinyin";
+  for (let key in data) {
+    const ele = data[key].element;
+    if (data[key][s].startsWith(curKey)) highlightElement(ele);
+    else delhighlightElement(ele);
+  }
+};
+
+const highlightElement = (ele) => {
+  ele.setAttribute("class", "form-input-box heightlight");
+};
+
+const delhighlightElement = (ele) => {
+  ele.setAttribute("class", "form-input-box");
+};
+
+const toggleWindow = (map) => {
+  if (isShow) {
+    closeWindow();
+  } else {
+    data = map;
+    const keys = generateKeysToBindings();
+    bindingKeysToData(keys);
+    openWindow();
+  }
+};
+
+const closeWindow = () => {
+  // 如果弹窗已经显示，则删除弹窗元素并将标记变量设置为 false
+  windowElement.parentNode.removeChild(windowElement);
+  isShow = false;
+  chrome.storage.sync.set(data);
+  data = {};
+  curKey = "";
+  mode = 0;
+};
+
+const openWindow = () => {
+  activeElement = document.activeElement;
+  windowElement = document.createElement("div");
+  windowElement.id = "my-popup";
+  updateWindowContent(windowElement, data);
+  document.body.appendChild(windowElement);
+  isShow = true;
+  return windowElement;
+};
+
+const updateWindowContent = (windowElement, data) => {
+  windowElement.innerHTML = "";
+  //一列最多放25行
+  let maxRowPerCol = 25;
+  let keys = Object.keys(data);
+  let colCount = Math.ceil(keys.length / maxRowPerCol);
+  for (let col = 0; col < colCount; col++) {
+    const colDiv = bulidColumn(maxRowPerCol, col, data);
+    windowElement.appendChild(colDiv);
+  }
+};
+
+const bulidColumn = (maxRowPerCol, col, data) => {
+  const colDiv = document.createElement("div");
+  colDiv.setAttribute("class", "column");
+  for (let row = 0; row < maxRowPerCol; row++) {
+    let dataItemKey = Object.keys(data)[col * maxRowPerCol + row];
+    if (!dataItemKey) {
+      break;
+    }
+    let name = data[dataItemKey].name;
+    const item = document.createElement("div");
+    item.setAttribute("class", "form-input-box");
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      inputContentToActiveElement(data[dataItemKey]);
+    });
+    data[dataItemKey].element = item;
+    item.innerHTML = `
+                <abcd class="key">${data[dataItemKey].key}</abcd>
+                <abcd class="value">${name}</abcd>
+            `;
+    colDiv.appendChild(item);
+  }
+  return colDiv;
+};
+
+const toggleSearchMode = () => {
+  curKey = "";
+  if (mode == 1) {
+    mode = 0;
+    windowElement.style.backgroundColor = "#fff";
+  } else {
+    mode = 1;
+    windowElement.style.backgroundColor = "#e1e1e1";
+  }
+};
+
+const generateKeysToBindings = () => {
+  let count = Object.keys(data).length;
+  let preffix = "";
+  let keys = [];
+  let p = 0;
+  while (true) {
+    for (let i = 0; i < bindings.length; i++) {
+      keys.push(preffix + bindings[i]);
+      p++;
+      if (p === count) {
+        return keys;
+      }
+    }
+    preffix = keys.shift();
+    p--;
+  }
+};
+
+const bindingKeysToData = (bindingKeys) => {
+  let p = 0;
+  for (let k in data) {
+    let key = bindingKeys[p++];
+    data[k]["key"] = key;
+    key2IndexMap[key] = k;
+  }
+};
+
+const inputContentToActiveElement = (value) => {
+  value.count++;
+  if (!activeElement) return;
+  activeElement.value = value.content;
+  activeElement.dispatchEvent(new Event("change"));
+  activeElement.dispatchEvent(new Event("input"));
+  toggleWindow();
+};
